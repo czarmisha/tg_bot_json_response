5502b90561cc0f63c58cd5464bf07ba3edf62ad1
czar_code <vertigomisha@gmail.com>
2020-10-27 15:56:38 +0500

    try to change static root dir

--- tb_test/urls.py
+++ tb_test/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
+from tb_test import settings
 from .views import TutorialBotView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', csrf_exempt(TutorialBotView.as_view())),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
